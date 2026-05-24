83ce00ece0b5244881aad320c6bef2d9b093f229
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2026-05-25 00:13:34 +0800

    删除CAI库

--- editor/plugins/AITool/CoronaArtificialIntelligence/ai_modules/three_d_generate/configs/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .dataclasses import Rodin3DSettings, Hunyuan3DSettings
-
-__all__ = ["Rodin3DSettings", "Hunyuan3DSettings"]
--- editor/plugins/AITool/CoronaArtificialIntelligence/ai_modules/three_d_generate/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .model_tools import load_3d_tools, load_hunyuan3d_tools
-
-__all__ = ["load_3d_tools", "load_hunyuan3d_tools"]
