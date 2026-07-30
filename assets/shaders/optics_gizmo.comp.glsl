543b3606b9dff289ec8c14e2949542cf73a782af
JOPLOPOL <95957645+JOPLOPOL@users.noreply.github.com>
2026-07-30 10:50:46 +0800

    temp

--- assets/shaders/optics_gizmo.comp.glsl
+++ assets/shaders/optics_gizmo.comp.glsl
@@ -24,7 +24,7 @@ layout(push_constant) uniform PushConsts {
 } pushConsts;
 
 layout(set = 0, binding = 0) uniform sampler2D textures[];
-layout(set = 2, binding = 0, rgba16) uniform image2D images[];
+layout(set = 2, binding = 0, rgba16f) uniform image2D images[];
 
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
